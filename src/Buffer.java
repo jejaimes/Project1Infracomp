fa9b929064773f7c871fdde7eb9117418286a9a9
JULIAN EDUARDO JAIMES CASTELLANOS <je.jaimes@sis.virtual.uniandes.edu.co>
2020-02-10 13:54:57 -0500

    avance

--- src/Buffer.java
+++ src/Buffer.java
@@ -8,4 +8,26 @@ public class Buffer {
 		this.size = size;
 		messages = new Message[size];
 	}
+	
+	public synchronized void saveMessage(Message m) {
+		if(size > 0) {
+			size--;
+			messages[size] = m;
+			try {
+				m.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			Thread.yield();
+		}
+		
+	}
+	
+	public synchronized Message sendMessage() {
+		Message m = messages[messages.length-1];
+		size++;
+		return m;
+	}
 }
