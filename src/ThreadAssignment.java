739e33c5ab9f4940f5cdc48a159b497d70854f9d
Dazinga <af.daza@uniandes.edu.co>
2020-02-20 17:19:06 -0500

    commented code deleted

--- src/ThreadAssignment.java
+++ src/ThreadAssignment.java
@@ -56,16 +56,7 @@ private static Client[] clients;
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//        Buffer buffer = new Buffer(10, 2);
-//        Server server1 = new Server(1, buffer);
-//        Server server2 = new Server(2, buffer);
-//        Client client1 = new Client(1,2, buffer);
-//        Client client2 = new Client(2,2, buffer);
-//        
-//        client1.start();
-//        client2.start();
-//        server1.start();
-//        server2.start();
+
         
     }
     
